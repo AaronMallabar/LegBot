97aad621d0c6951d16ab514bbb357b69a8646496
Aaron <me@spectre.wireless.rit.edu>
2018-04-10 14:31:11 -0400

    Deleted everything just to reinit

--- Leg_Data_collect/Functions/disconnectBioRadio.m
+++
@@ -1,7 +0,0 @@
-function disconnectBioRadio(bioRadioHandle)
-
-   % Stop Communication
-   BioRadio150_Stop(bioRadioHandle);
-   
-   % Remove BioRadio Info. from MATLAB memory
-   BioRadio150_Unload(bioRadioHandle);
--- Leg_Data_collect/Functions/initGlobals.m
+++
@@ -1,5 +0,0 @@
-function initGlobals()
-
-global bioRadioHandle isCollecting;
-bioRadioHandle = -1;
-isCollecting = 0;
