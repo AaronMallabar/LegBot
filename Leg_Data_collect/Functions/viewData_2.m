93b332c767c22c7e42e51f22fe5cf52e6c81a2a6
Aaron <amallabar13@gmail.com>
2018-04-13 02:11:33 -0400

    Everything is modular now, finished up SVM

--- Leg_Data_collect/Functions/viewData_2.m
+++ Leg_Data_collect/Functions/viewData_2.m
@@ -15,24 +15,25 @@ load('AutoLegData.mat');
 %% Plotting Raw Data
 FirstPlot = 1;
 LastPlot = 40;
-%figureNum = plotData(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, FirstPlot, LastPlot, figureNum);
+figureNum = plotData(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, FirstPlot, LastPlot, figureNum);
 
 %% Wavelets
-NumWaveletSamples = 5;  %Higher the number the longer it takes, plan about 30 sec per sample. 
-% [kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
-%     wavelets(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, NumWaveletSamples, figureNum);
+NumWaveletSamples = 2;  %Higher the number the longer it takes, plan about 30 sec per sample. 
+[kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
+    wavelets(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, NumWaveletSamples, figureNum);
 
 %% Calculate FFT's
 
-% [kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
-%     CalculateFFT(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, figureNum);
+[kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
+    CalculateFFT(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, figureNum);
 
 %% Power Spectral Density
 
-% [kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
-%     PSD(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, figureNum);
+[kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
+    PSD(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, figureNum);
 
 %% SVM
 
-%SVM();
+[kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest,figureNum] = ...
+    SVM(kickOut, kickIn, Dorsiflexion, Plantarflexion, Rest, figureNum, numSamples, numClasses);
 %SVM3();
